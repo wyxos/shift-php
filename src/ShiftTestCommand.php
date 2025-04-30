d8b6cb0669e2d6678c9e03216d91b96132930c61
Joey J <joey@wyxos.com>
2025-04-30 06:16:38 +0400

    Initial commit

---
+++ src/ShiftTestCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Wyxos\Shift;
+
+use Illuminate\Console\Command;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Facades\Http;
+
+class ShiftTestCommand extends Command
+{
+    protected $signature = 'shift:test';
+
+    protected $description = 'Test SHIFT SDK by creating a dummy task.';
+
+    /**
+     * @throws ConnectionException
+     */
+    public function handle()
+    {
+        $projectId = config('services.shift.project_id');
+        $token = config('services.shift.api_token');
+        $baseUrl = config('services.shift.url');
+
+        $response = Http::withToken($token)
+            ->acceptJson()
+            ->post($baseUrl . '/api/tasks', [
+                'name' => 'Test Task',
+                'description' => 'This is a test task created by the installer',
+                'project_id' => $projectId
+            ]);
+
+        if ($response->successful()) {
+            $this->info('Test task created successfully.');
+        } else {
+            $this->error('Failed to create test task: ' . $response->body());
+        }
+    }
+}
