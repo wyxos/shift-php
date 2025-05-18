729b05a472177e286323a67b11a2168187069332
Joey J <github@wyxos.com>
2025-05-18 10:45:38 +0400

    Refactor Dashboard and API routes; integrate Axios for task fetching

--- ui/src/router.ts
+++ ui/src/router.ts
@@ -1,16 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Dashboard from './components/Dashboard.vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HelloWorld,
-    props: { msg: 'Vite + Vue' }
-  },
-  {
-    path: '/dashboard',
     name: 'dashboard',
     component: Dashboard
   }
