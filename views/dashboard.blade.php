0b0007c969197591b6d4fb8e7fd771096390bddd
Joey J <joey@wyxos.com>
2025-05-18 04:01:10 +0400

    Update dashboard layout and enforce HTTPS scheme in AppServiceProvider

--- views/dashboard.blade.php
+++ views/dashboard.blade.php
@@ -4,10 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Shift Dashboard</title>
-        @vite('resources/shift/app.js')
     </head>
 
     <body>
-        <div id="app"></div>
+        <div id="app">
+            SHIFT
+        </div>
     </body>
 </html>
