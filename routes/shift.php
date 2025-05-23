714ddc636bb6f076d775a606ed4933ffd5b9ec4a
Joey J <github@wyxos.com>
2025-05-23 19:01:05 +0400

    feat: add route names for task and settings API endpoints

--- routes/shift.php
+++ routes/shift.php
@@ -5,10 +5,13 @@ use Wyxos\Shift\Http\Controllers\ShiftController;
 use Wyxos\Shift\Http\Controllers\TaskController;
 
 Route::middleware(['web', 'auth'])->group(function () {
-    Route::get('/shift', [ShiftController::class, 'index']);
 
-    Route::get('/shift/api/tasks', [TaskController::class, 'index'])->name('shift.tasks.index');
-    Route::get('/shift/api/tasks/{id}', [TaskController::class, 'show'])->name('shift.tasks.show');
-    Route::post('/shift/api/tasks', [TaskController::class, 'store'])->name('shift.tasks.store');
-    Route::put('/shift/api/tasks/{id}', [TaskController::class, 'update'])->name('shift.tasks.update');
+    Route::get('/shift/api/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/shift/api/tasks/{id}', [TaskController::class, 'show'])->name('tasks.show');
+    Route::post('/shift/api/tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::put('/shift/api/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
+
+    Route::get('/shift/{page?}', [ShiftController::class, 'index'])
+        ->name('shift.dashboard')
+        ->where('page', '.*');
 });
