764eadf8ec357a2c9433f7f0db24cad73e29bffd
Joey J <github@wyxos.com>
2025-05-25 05:51:26 +0400

    feat: update SHIFT configuration handling and improve error responses

--- routes/shift.php
+++ routes/shift.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use Wyxos\Shift\Http\Controllers\ShiftController;
-use Wyxos\Shift\Http\Controllers\TaskController;
+use Wyxos\Shift\Http\Controllers\ShiftTaskController;
 
 Route::middleware(['web', 'auth'])->group(function () {
 
-    Route::get('/shift/api/tasks', [TaskController::class, 'index'])->name('tasks.index');
-    Route::get('/shift/api/tasks/{id}', [TaskController::class, 'show'])->name('tasks.show');
-    Route::post('/shift/api/tasks', [TaskController::class, 'store'])->name('tasks.store');
-    Route::put('/shift/api/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::get('/shift/api/tasks', [ShiftTaskController::class, 'index'])->name('tasks.index');
+    Route::get('/shift/api/tasks/{id}', [ShiftTaskController::class, 'show'])->name('tasks.show');
+    Route::post('/shift/api/tasks', [ShiftTaskController::class, 'store'])->name('tasks.store');
+    Route::put('/shift/api/tasks/{id}', [ShiftTaskController::class, 'update'])->name('tasks.update');
 
     Route::get('/shift/{page?}', [ShiftController::class, 'index'])
         ->name('shift.dashboard')
