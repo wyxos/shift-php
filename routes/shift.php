ff7c14849f6ecc1c1fde7d145c19dd5bb6fa0060
Joey J <github@wyxos.com>
2025-05-25 13:14:40 +0400

    feat: add delete functionality for tasks in API and UI

--- routes/shift.php
+++ routes/shift.php
@@ -10,6 +10,7 @@ Route::middleware(['web', 'auth'])->group(function () {
     Route::get('/shift/api/tasks/{id}', [ShiftTaskController::class, 'show'])->name('tasks.show');
     Route::post('/shift/api/tasks', [ShiftTaskController::class, 'store'])->name('tasks.store');
     Route::put('/shift/api/tasks/{id}', [ShiftTaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/shift/api/tasks/{id}', [ShiftTaskController::class, 'destroy'])->name('tasks.destroy');
 
     Route::get('/shift/{page?}', [ShiftController::class, 'index'])
         ->name('shift.dashboard')
