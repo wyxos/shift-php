49bdab177bdf92c8d56f2bf1085ced9622f4c45b
Wyxos <github@wyxos.com>
2025-05-31 21:20:40 +0400

    feat: enhance styling and structure in App.vue and EditTask.vue for better user experience

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -8,7 +8,7 @@ const appUrl = window.shiftConfig.baseUrl;
 const appName = window.shiftConfig.appName;
 </script>
 <template>
-    <div class="flex flex-col items-center overflow-hidden p-4 md:h-screen">
+    <div class="flex flex-col items-center overflow-hidden bg-blue-100 p-4 md:h-screen">
         <a :href="appUrl" class="p-4 font-bold hover:text-blue-500">{{ appName }}</a>
         <router-view></router-view>
         <AuthErrorModal :redirect-url="loginUrl" />
