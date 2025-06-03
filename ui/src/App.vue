3c7cccfd6ee6277ecacc6903781e35257f63b8a4
Wyxos <github@wyxos.com>
2025-06-03 18:44:04 +0400

    feat: enhance user experience by displaying logged-in username and refactor edit task navigation

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,14 +2,18 @@
 import AuthErrorModal from './components/AuthErrorModal.vue';
 
 const loginUrl = window.shiftConfig.loginRoute;
-
 const appUrl = window.shiftConfig.baseUrl;
-
 const appName = window.shiftConfig.appName;
+const username = window.shiftConfig.username;
 </script>
 <template>
     <div class="flex flex-col items-center overflow-hidden bg-blue-100 p-4 md:h-screen">
-        <a :href="appUrl" class="p-4 font-bold hover:text-blue-500">{{ appName }}</a>
+        <div class="w-full flex justify-between items-center px-4">
+            <a :href="appUrl" class="p-4 font-bold hover:text-blue-500">{{ appName }}</a>
+            <div v-if="username" class="text-sm text-gray-600">
+                Logged in as: <span class="font-semibold">{{ username }}</span>
+            </div>
+        </div>
         <router-view></router-view>
         <AuthErrorModal :redirect-url="loginUrl" />
     </div>
