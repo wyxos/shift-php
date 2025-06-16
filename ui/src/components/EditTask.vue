d3aa84b9355b3d6fbdbfb398ffd9b12098419305
Wyxos <wyxos@proton.me>
2025-06-16 13:21:38 +0400

    feat: update version to 1.0.75 and add type assertion for editor elements in CreateTask.vue and EditTask.vue

--- ui/src/components/EditTask.vue
+++ ui/src/components/EditTask.vue
@@ -362,7 +362,7 @@ onMounted(() => {
             });
 
             // Add event listener to adjust height based on content
-            const editorEl = messageEditorContainerRef.value.querySelector('.toastui-editor-main .ProseMirror');
+            const editorEl = messageEditorContainerRef.value.querySelector('.toastui-editor-main .ProseMirror') as HTMLElement;
             if (editorEl) {
                 const observer = new MutationObserver(() => {
                     const editorHeight = editorEl.scrollHeight;
@@ -413,7 +413,7 @@ onMounted(() => {
                 });
 
                 // Add event listener to adjust height based on content
-                const editorEl = descriptionEditorContainerRef.value.querySelector('.toastui-editor-main .ProseMirror');
+                const editorEl = descriptionEditorContainerRef.value.querySelector('.toastui-editor-main .ProseMirror') as HTMLElement;
                 if (editorEl) {
                     const observer = new MutationObserver(() => {
                         const editorHeight = editorEl.scrollHeight;
