e7c73b0e81a8ea10f77eb7f62079acb5da2e5e4a
Wyxos <wyxos@proton.me>
2026-07-09 17:26:45 +0400

    fix: brand task thread reply subjects

--- src/TaskThreadUpdated.php
+++ src/TaskThreadUpdated.php
@@ -37,12 +37,11 @@ class TaskThreadUpdated extends CoreTaskThreadUpdated
     public function toMail(object $notifiable): MailMessage
     {
         $taskTitle = $this->data['task_title'] ?? 'Task #'.$this->data['task_id'];
-        $threadType = ucfirst($this->data['type']).' thread';
         $snippet = Str::limit($this->previewContent(), 120);
         $url = $this->resolveUrl();
 
         $message = (new MailMessage)
-            ->subject("New reply in {$threadType} for {$taskTitle}")
+            ->subject("SHIFT: New reply on {$taskTitle}")
             ->line('A new message was posted.')
             ->line("Preview: \"{$snippet}\"")
             ->markdown('shift::notifications.email');
